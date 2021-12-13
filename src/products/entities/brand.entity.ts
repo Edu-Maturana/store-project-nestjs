a64579878b53e7318008f241e73b8a331c8fbca4
Eduardo Maturana <edu@Mac-mini-de-Eduardo.local>
2021-12-13 15:29:58 -0300

    add entities to type-orm

--- src/products/entities/brand.entity.ts
+++ src/products/entities/brand.entity.ts
@@ -1,5 +1,13 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Brand {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type: 'varchar', length: 100 })
   name: string;
+
+  @Column({ type: 'varchar', length: 255 })
   image: string;
 }
