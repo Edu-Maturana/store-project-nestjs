4d14e3b370413dda0dac20893bf7f14a9ee6efbb
Eduardo Maturana <edu@Mac-mini-de-Eduardo.local>
2021-12-27 18:08:08 -0300

    relation brands to products

--- src/products/services/products.service.ts
+++ src/products/services/products.service.ts
@@ -3,16 +3,20 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import { Product } from './../entities/product.entity';
+import { BrandsService } from './brands.service';
 import { CreateProductDto, UpdateProductDto } from './../dtos/products.dtos';
 
 @Injectable()
 export class ProductsService {
   constructor(
     @InjectRepository(Product) private productsRepo: Repository<Product>,
+    private brandsService: BrandsService,
   ) {}
 
   findAll() {
-    return this.productsRepo.find();
+    return this.productsRepo.find({
+      relations: ['brand'],
+    });
   }
 
   async findOne(id: number) {
@@ -23,13 +27,21 @@ export class ProductsService {
     return product;
   }
 
-  create(data: CreateProductDto) {
+  async create(data: CreateProductDto) {
     const newProduct = this.productsRepo.create(data);
+    if (data.brandId) {
+      const brand = await this.brandsService.findOne(data.brandId);
+      newProduct.brand = brand;
+    }
     return this.productsRepo.save(newProduct);
   }
 
   async update(id: number, changes: UpdateProductDto) {
     const product = await this.productsRepo.findOne(id);
+    if (changes.brandId) {
+      const brand = await this.brandsService.findOne(changes.brandId);
+      product.brand = brand;
+    }
     this.productsRepo.merge(product, changes);
     return this.productsRepo.save(product);
   }
