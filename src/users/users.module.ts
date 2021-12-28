571bcbd86db45c07de206c1b9e3df27d0510847a
Eduardo Maturana <edu@Mac-mini-de-Eduardo.local>
2021-12-28 19:40:26 -0300

    custom relations for orders

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,14 +4,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CustomerController } from './controllers/customers.controller';
 import { CustomersService } from './services/customers.service';
 import { Customer } from './entities/customer.entity';
+
 import { UsersController } from './controllers/users.controller';
 import { UsersService } from './services/users.service';
 import { User } from './entities/user.entity';
 
+import { Order } from './entities/order.entity';
+import { OrderItem } from './entities/order-item.entity';
+
 import { ProductsModule } from '../products/products.module';
 
 @Module({
-  imports: [ProductsModule, TypeOrmModule.forFeature([Customer, User])],
+  imports: [
+    ProductsModule,
+    TypeOrmModule.forFeature([Customer, User, Order, OrderItem]),
+  ],
   controllers: [CustomerController, UsersController],
   providers: [CustomersService, UsersService],
 })
